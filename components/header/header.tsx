bd47cb12f4b9c03cf511965ddc3ad5564c0c873e
Sirio2022 <105917953+Sirio2022@users.noreply.github.com>
2024-01-13 08:19:01 -0500

    01-Create-Next-App-Layout

---
+++ components/header/header.tsx
@@ -0,0 +1,30 @@
+import Link from 'next/link'
+import React from 'react'
+
+export default function Header() {
+    return (
+        <header>
+            <nav>
+                <div className='navbar justify-between bg-base-300'>
+                    <Link href='/' className='btn btn-ghost text-lg'>
+                        Amazing Ecommerce
+                    </Link>
+
+                    <ul className='flex'>
+                        <li>
+                            <Link href='/cart' className='btn btn-ghost rounded-btn'>
+                                Cart
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href='/login' className='btn btn-ghost rounded-btn'>
+                                Login
+                            </Link>
+                        </li>
+                    </ul>
+
+                </div>
+            </nav>
+        </header>
+    )
+}
