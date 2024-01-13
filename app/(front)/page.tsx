6b8529fd2c3ac7cffec0d8f8c0cf524adca5f47b
Sirio2022 <105917953+Sirio2022@users.noreply.github.com>
2024-01-13 09:57:34 -0500

    Beautiful!

--- app/(front)/page.tsx
+++ app/(front)/page.tsx
@@ -1,3 +1,5 @@
+import ProductItem from "@/components/products/ProductItem";
+import data from "@/lib/data";
 
 
 export default function Home() {
@@ -6,6 +8,12 @@ export default function Home() {
       <h2 className='text-2xl py-2'>
         Latest Products
       </h2>
+
+      <div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4'>
+        {
+          data.products.map((product) => <ProductItem product={product} key={product.slug} />)
+        }
+      </div>
     </>
   )
 }
