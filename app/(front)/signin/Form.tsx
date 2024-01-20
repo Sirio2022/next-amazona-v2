581204e8a34023ad5155222c456437505e19f771
Sirio2022 <105917953+Sirio2022@users.noreply.github.com>
2024-01-20 08:40:39 -0500

    User Auth Ready!  Nice!

--- app/(front)/signin/Form.tsx
+++ app/(front)/signin/Form.tsx
@@ -17,7 +17,7 @@ export default function Form() {
 
     const params = useSearchParams()
 
-    let callbackUrl = params.get('callbackUrl' || '/') as string
+    let callbackUrl = params.get('callbackUrl') || '/'
 
     const router = useRouter()
 
@@ -56,7 +56,83 @@ export default function Form() {
                     Sign in
                 </h1>
 
-            </div>
-        </div>
+                {
+                    params.get('error') && (
+                        <div className="alert text-error flex justify-center items-center">
+                            {params.get('error') === 'CredentialsSignin' ? 'Invalid credentials' : params.get('error')}
+
+                        </div>
+                    )
+                }
+                {
+                    params.get('success') && (
+                        <div className="alert alert-success">
+                            {params.get('success')}
+                        </div>
+                    )
+                }
+                <form onSubmit={handleSubmit(formSubmit)}>
+
+                    <div className="my-2">
+                        <label htmlFor="email" className="label">
+                            Email
+                        </label>
+                        <input
+                            type="email"
+                            id="email"
+                            {...register("email", {
+                                required: 'Email is required',
+                                pattern: {
+                                    value: /[a-z0-9]+@[a-z]+\.[a-z]/,
+                                    message: 'Email is not valid'
+                                },
+                            })}
+                            className="input input-bordered max-w-sm w-full"
+                        />
+                        {errors.email?.message && (
+                            <div className="text-error flex justify-center items-center">
+                                {errors.email.message}
+                            </div>
+
+                        )}
+                    </div>
+
+                    <div className="my-2">
+                        <label htmlFor="password" className="label">
+                            Password
+                        </label>
+                        <input
+                            type="password"
+                            id="password"
+                            {...register("password", { required: 'Password is required' })}
+                            className="input input-bordered w-full max-w-sm"
+                        />
+                        {errors.password?.message && (
+                            <div className="text-error">
+                                {errors.password.message}
+                            </div>
+
+                        )}
+
+                    </div>
+
+                    <div className="my-4">
+                        <button
+                            type="submit"
+                            className="btn btn-primary w-full"
+                            disabled={isSubmitting}
+                        >
+                            {isSubmitting && (
+                                <span className="loading loading-spinner"></span>
+                            )}
+                            Sign in
+                        </button>
+                    </div>
+
+                </form>
+
+
+            </div >
+        </div >
     )
 }
