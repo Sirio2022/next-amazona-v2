8695771d39e346758dd665b7925e39e5801cb59e
Juan Manuel Alvarez Bermudez <105917953+Sirio2022@users.noreply.github.com>
2025-03-15 11:14:07 -0500

    Va quedando hermoso!
    Uich que belleza!

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,41 +1,41 @@
-import CredentialsProvider from 'next-auth/providers/credentials';
-import bcrypt from 'bcryptjs';
-import dbConnect from './dbConnect';
-import UserModel from './models/UserModel';
-import NextAuth from 'next-auth';
+import CredentialsProvider from 'next-auth/providers/credentials'
+import bcrypt from 'bcryptjs'
+import dbConnect from './dbConnect'
+import UserModel from './models/UserModel'
+import NextAuth from 'next-auth'
 
 export const config = {
   providers: [
     CredentialsProvider({
       credentials: {
         email: {
-          type: 'email',
+          type: 'email'
         },
-        password: { type: 'password' },
+        password: { type: 'password' }
       },
       async authorize(credentials) {
-        await dbConnect();
-        if (credentials == null) return null;
+        await dbConnect()
+        if (credentials == null) return null
 
-        const user = await UserModel.findOne({ email: credentials.email });
+        const user = await UserModel.findOne({ email: credentials.email })
 
         if (user) {
           const isMatch = await bcrypt.compare(
             credentials.password as string,
             user.password
-          );
+          )
           if (isMatch) {
-            return user;
+            return user
           }
         }
-        return null;
-      },
-    }),
+        return null
+      }
+    })
   ],
   pages: {
     signIn: '/signin',
     newUser: '/register',
-    error: '/signin',
+    error: '/signin'
   },
   callbacks: {
     async jwt({ user, trigger, session, token }: any) {
@@ -44,30 +44,30 @@ export const config = {
           _id: user._id,
           email: user.email,
           name: user.name,
-          isAdmin: user.isAdmin,
-        };
+          isAdmin: user.isAdmin
+        }
       }
       if (trigger === 'update' && session) {
         token.user = {
           ...token.user,
           email: session.user.email,
-          name: session.user.name,
-        };
+          name: session.user.name
+        }
       }
-      return token;
+      return token
     },
     session: async ({ session, token }: any) => {
       if (token) {
-        session.user = token.user;
+        session.user = token.user
       }
-      return session;
-    },
-  },
-};
+      return session
+    }
+  }
+}
 
 export const {
   handlers: { GET, POST },
   auth,
   signIn,
-  signOut,
-} = NextAuth(config);
+  signOut
+} = NextAuth(config)
